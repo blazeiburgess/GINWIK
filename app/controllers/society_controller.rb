2aca01bd59b46248e867d8764fb6d173c60e0b38
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-07-21 13:48:56 -0400

    modified society controller to allow simpler templating

--- app/controllers/society_controller.rb
+++ app/controllers/society_controller.rb
@@ -1,14 +1,14 @@
 class SocietyController < Sinatra::Base
-  set :views, Proc.new { File.join(root, "../views/society") }
+  set :views, Proc.new { File.join(root, "../views") }
   enable :sessions 
 
   get '/societies' do 
-    erb :index
+    erb :'society/index'
   end
 
   get '/societies/:slug' do
     @society = Society.find(params[:slug].split("_")[-1])
     @methods = [:culture, :economic_organization, :education, :environment, :governmental_organization, :history, :language, :technological_development]
-    erb :show
+    erb :'society/show'
   end
 end
