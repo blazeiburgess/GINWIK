b1cb71582afcfaf1572e5e8937072d105bb846c7
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-07-21 11:07:06 -0400

    changed model relations to fix relational structure

--- app/models/society.rb
+++ app/models/society.rb
@@ -1,5 +1,13 @@
 class Society < ActiveRecord::Base
   has_many :social_groups
   belongs_to :user
-
+  has_one :technological_development
+  has_one :culture
+  has_one :economic_organization
+  has_one :education
+  has_one :environment
+  has_one :governmental_organization
+  has_one :history
+  has_one :language
+  has_one :social_group 
 end
