6657cd35c8448ba93af352f404b07d1e7e7fa330
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-07-22 11:24:18 -0400

    fixed user edit/new routes' input handling

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,17 +2,24 @@
 
 class UserController < SocietyController
   post '/users' do
-    user = User.create(params[:user])
-    session[:user_id] = user.id
+    user = User.find(params[:user_id])
+    binding.pry 
+    user.update(params[:user])
     redirect to "/users/#{user.id}"
   end
 
   get '/users/new' do
-    erb :'user/new'
+    @user = User.create
+    erb :'user/edit'
   end 
 
   get '/users/:id' do
     @user = User.find(params[:id])
     erb :'user/show'
+  end
+
+  get '/users/:id/edit' do
+    @user = User.find(params[:id])
+    erb :'user/edit'
   end
 end
