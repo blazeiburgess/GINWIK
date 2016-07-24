f0fd0c29f69956c2782afd2a7c8c2d53fd8881e1
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-07-24 16:58:44 -0400

    finished clone method by adding handling for social groups and conflicts

--- app/controllers/clone_controller.rb
+++ app/controllers/clone_controller.rb
@@ -10,7 +10,18 @@ class CloneController < ApplicationController
 	@society.send("#{method}=", get_class_name(method.to_s).create(info))
       end
     end 
-    
+    @old_society.social_groups.each do |social_group|
+      info = social_group.attributes
+      info.delete("id")
+      info["society_id"] = @society.id 
+      @society.social_groups << SocialGroup.create(info)
+    end    
+    @old_society.conflicts.each do |conflict|
+      info = conflict.attributes
+      info.delete("id")
+      info["society_id"] = @society.id 
+      @society.conflicts << Conflict.create(info)
+    end    
     redirect to "/societies/#{get_slug(@society)}"
   end
 
