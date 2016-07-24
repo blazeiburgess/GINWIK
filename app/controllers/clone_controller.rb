1fab74e74a8a2991ebad6b6883c4ad97ff68aeb8
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-07-24 16:14:11 -0400

    fixed clone bug that was destructive of original information

--- app/controllers/clone_controller.rb
+++ app/controllers/clone_controller.rb
@@ -3,9 +3,14 @@ class CloneController < ApplicationController
     @old_society = Society.find(params[:society_id])
     @society = Society.create(params[:society]) 
     METHODS.each do |method|
-      @society.send("#{method}=", @old_society.send(method))
-    end
-    @society.save
+      if @old_society.send(method)
+	info = @old_society.send(method).attributes	
+	info.delete("id")
+	info.delete("society_id")
+	@society.send("#{method}=", get_class_name(method.to_s).create(info))
+      end
+    end 
+    
     redirect to "/societies/#{get_slug(@society)}"
   end
 
