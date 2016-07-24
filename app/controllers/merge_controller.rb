8dc6c8136f8bdb70841ea514af1cbc02deb6fbb6
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-07-24 15:03:23 -0400

    fixed merge method to be non-destructive of original information and modifiable in transit

--- app/controllers/merge_controller.rb
+++ app/controllers/merge_controller.rb
@@ -1,18 +1,19 @@
 class MergeController < ApplicationController
   post '/merge/:method' do
     @society = Society.find(get_params_method(params)[:society_id])
+    # @old_method = get_class_name(params[:old_method_id]).find
     if @society.send(params[:method].to_sym)
-      @method = get_class_name(params[:method]).update(get_params_method(params))     
+      @society.send(params[:method].to_sym).update(get_params_method(params))
     else
-      @method = get_class_name(params[:method]).create(get_params_method(params))    
-      @method.save
+      @society.send("#{params[:method].to_sym}=", get_class_name(params[:method]).new(get_params_method(params)))
+      @society.send(params[:method].to_sym).save
+      # @society.save
     end
-    redirect to "/societies/#{get_slug(@method.society)}/#{params[:method]}"
+    redirect to "/societies/#{get_slug(@society)}/#{params[:method]}"
   end
 
   get '/merge/:method/:id' do
     if is_logged_in?(session)
-      
       @method = get_class_name(params[:method]).find(params[:id])
       "#{@method.description}"
       erb :merge
