cef84cf50fedc05afc2d1e96f2caf9eba7fed3b1
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-07-25 14:33:26 -0400

    added public folder styling, removed embedded styles from layout, and began loading Skeleton locally

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < Sinatra::Base
   include Helpers
   set :views, Proc.new { File.join(root, "../views/") }
+  set :public_folder, Proc.new { File.join(root, "../public/") }
   enable :sessions
   set :session_secret, 'super secret'
 
